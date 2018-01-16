638085fa9449f6cd5a4ed3dd2f4588c6456fc48f
0918nobita <nobita.0918@gmail.com>
2018-01-16 22:43:51 +0900

    リストの指定した位置の要素を取得する述語 retrieve を追加した

--- list.pl
+++ list.pl
@@ -1,10 +1,14 @@
 % リスト操作
 
 % 先頭要素を取得する
-car([X | Ys], X).
+car([Car | Cdr], Car).
 
 % 先頭の要素を除いた残りの要素のリストを取得する
-cdr([X | Ys], Ys).
+cdr([Car | Cdr], Cdr).
 
-% 先頭に要素を追加する
-cons(X, Ls, [X | Ls]).
+% リスト List の先頭に要素 Element を追加して新しいリストを作る
+cons(Element, List, [Element | List]).
+
+% リスト [Car | Cdr] の N 番目の要素を取得する
+retrieve([Car | Cdr], 1, Car).
+retrieve([Car | Cdr], N, X) :-　N > 1,　N1 is N - 1,　retrieve(Cdr, N1, X).

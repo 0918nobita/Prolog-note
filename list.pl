b0b1f708f42ba6caff8932f9f3c37c4bacc76945
0918nobita <nobita.0918@gmail.com>
2018-01-17 10:56:02 +0900

    nth0/3, nth1/3, nth0/4, nth1/4の説明を追加した

--- list.pl
+++ list.pl
@@ -12,3 +12,17 @@ cons(Element, List, [Element | List]).
 % リスト [Car | Cdr] の N 番目の要素を取得する
 retrieve([Car | Cdr], 1, Car).
 retrieve([Car | Cdr], N, X) :-　N > 1,　N1 is N - 1,　retrieve(Cdr, N1, X).
+
+% nth0(?Index, ?List, ?Elem)
+% Elem が リスト List の Index 番目の要素なら true。カウントは0から始まる。
+
+% nth1(?Index, ?List, ?Elem)
+% Elem がリスト List の Index 番目の要素なら true。カウントは1から始まる。
+
+% nth0(?N, ?List, ?Elem, ?Rest)
+% 指定した要素を取り出したり挿入したりする。
+% Elemが Listの N 番目の要素で、Rectがその残りのリストなら true。
+
+% nth1(?N, ?List, ?Elem, ?Rest)
+% nth0と同様だが、カウントが1から始まる。
+

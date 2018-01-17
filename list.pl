49d1d21afa79376f1593a5e92d0783d412359106
0918nobita <nobita.0918@gmail.com>
2018-01-17 11:26:18 +0900

    Remove singleton variables

--- list.pl
+++ list.pl
@@ -1,17 +1,17 @@
 % リスト操作
 
 % 先頭要素を取得する
-car([Car | Cdr], Car).
+car([Car|_], Car).
 
 % 先頭の要素を除いた残りの要素のリストを取得する
-cdr([Car | Cdr], Cdr).
+cdr([_|Cdr], Cdr).
 
 % リスト List の先頭に要素 Element を追加して新しいリストを作る
 cons(Element, List, [Element | List]).
 
 % リスト [Car | Cdr] の N 番目の要素を取得する
-retrieve([Car | Cdr], 1, Car).
-retrieve([Car | Cdr], N, X) :-　N > 1,　N1 is N - 1,　retrieve(Cdr, N1, X).
+retrieve([Car|_], 1, Car).
+retrieve([_|Cdr], N, X) :-　N > 1,　N1 is N - 1,　retrieve(Cdr, N1, X).
 
 % nth0(?Index, ?List, ?Elem)
 % Elem が リスト List の Index 番目の要素なら true。カウントは0から始まる。
